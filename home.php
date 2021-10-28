efa189769b220fdc950b0367fc1ca1b458ee3836
wuqianye <natsumi3613@gmail.com>
2021-10-28 18:24:02 -0400

    start html pages

--- home.php
+++ home.php
@@ -1,2 +1,14 @@
 <!-- home page with a navbar, a search bar, a addMember button and information about different types of membership -->
 <!-- navbar displaying only name -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
--- newMember.php
+++ newMember.php
@@ -1,2 +1,14 @@
 <!-- a page with a navbar, a form to collect new member information, and a submit button -->
 <!-- navbar with name and going back to homepage -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>New Member</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
