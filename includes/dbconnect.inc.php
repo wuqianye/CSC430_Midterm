c8bd1d3db2993576351c6c0b6497b0413cf4348e
wuqianye <natsumi3613@gmail.com>
2021-10-28 17:30:12 -0400

    add dbconnection

--- includes/dbconnect.inc.php
+++ includes/dbconnect.inc.php
@@ -1,1 +1,18 @@
 <!-- database connection -->
+
+<?php
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "CSC430Lab4";
+
+    try {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn->set_charset("utf8mb4");
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        exit("Error connecting to databse");
+    }
+?>
